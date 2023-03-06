651f9f32469495ead914da730f716f2461fe7884
davorpavlov <pavlovic.davor91@gmail.com>
2023-03-06 19:43:01 +0100

    hangman

---
+++ hangman.py
@@ -0,0 +1,89 @@
+import time
+import os
+
+def clear():
+    os.system('cls')
+
+def hangman_animation():
+    # Here you can define the animation frames
+    frames = [
+        r"""
+              +---+
+              |   |
+                  |
+                  |
+                  |
+                  |
+            =========""",
+        r"""
+              +---+
+              |   |
+              O   |
+                  |
+                  |
+                  |
+            =========""",
+        r"""
+              +---+
+              |   |
+              O   |
+              |   |
+                  |
+                  |
+            =========""",
+        r"""
+              +---+
+              |   |
+              O   |
+             /|   |
+                  |
+                  |
+            =========""",
+        r"""
+              +---+
+              |   |
+              O   |
+             /|\  |
+                  |
+                  |
+            =========""",
+        r"""
+              +---+
+              |   |
+              O   |
+             /|\  |
+             /    |
+                  |
+            =========""",
+        r"""
+              +---+
+              |   |
+              O   |
+             /|\  |
+             / \  |
+                  |
+            =========""",
+        r"""
+              +---+
+              |   |
+             \O/  |
+              |   |
+             / \  |
+                  |
+            =========""",
+        r"""
+              +---+
+              |   |
+             \O/  |
+              |   |
+             / \  |
+            _/   \_
+            =========""",
+    ]
+
+    for frame in frames:
+        clear()
+        print(frame)
+        time.sleep(1)  # Adjust the time delay as needed
+
+hangman_animation()
